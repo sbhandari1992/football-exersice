930a1388fa02691cf15161b8abb0ad78bc81680d
Sam Bhandari <sambhandari2021@yahooe.com>
2022-11-04 10:36:22 -0400

    practice adding  dropdown menu with js

---
+++ football.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    //create an array of football team
+    let teams  = [
+        { code: "DAL", name: "Dallas Cowboys", plays: "Arlington, TX" },
+        { code: "DEN", name: "Denver Broncos", plays: "Denver, CO" },
+        { code: "HOU", name: "Houston Texans", plays: "Houston, TX" },
+        { code: "KAN", name: "Kansas City Chiefs", plays: "Kansas City, MO" }
+    ];
+    let selectMenu = document.getElementById('teams');
+    console.log(`the select menu is ${selectMenu}`);
+    console.log(selectMenu);
+    let moreInfoBtn = document.getElementById('teams');
+    console.log(`the select menu is ${selectMenu}`);
+
+}
