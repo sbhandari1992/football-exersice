068ae5482d2f9686d234d377828c5399106bb3d9
Sam Bhandari <sambhandari2021@yahooe.com>
2022-11-04 22:09:06 -0400

    practice at class

--- football.js
+++ football.js
@@ -9,7 +9,53 @@ window.onload = function() {
     let selectMenu = document.getElementById('teams');
     console.log(`the select menu is ${selectMenu}`);
     console.log(selectMenu);
-    let moreInfoBtn = document.getElementById('teams');
-    console.log(`the select menu is ${selectMenu}`);
+    let moreInfoBtn = document.getElementById('moreInfo');
+    console.log(`the select menu is ${moreInfoBtn}`);
+    console.log(moreInfoBtn);
+
+    let numOfTeams = teams.length;
+    console.log(`Number of team is: ${numOfTeams}`);
+    console.log(numOfTeams);
+    for (let i = 0; i < numOfTeams; i++) {
+        let theOption = new Option(teams[i].name,teams[i].code);
+        console.log(theOption);
+        selectMenu.appendChild(theOption);
+    }
+
+    ///add an event listener
+    moreInfoBtn.addEventListener('click', getTeamName);
+
+
+    //add an event handler
+    function getTeamName() {
+        console.log('button works');
+        let teamCode = selectMenu.value;
+        console.log(teamCode);
+        let teamName = selectMenu.options[selectMenu.selectedIndex].text;
+        console.log(`you select ${teamName} with code ${teamCode}`)
+
+    }
+
+    ///remove an option from list
+    // let itemToDelete = "DAL";
+    // let numberOfOptionsTags = selectMenu.options.length;
+    // for (let i = 0; i < numberOfOptionsTags; i++) {
+    //     if (selectMenu.options[i].value == itemToDelete) {
+    //         selectMenu.remove(i);
+    //         break;  
+    //        }
+
+    // }
+    
+    ///this is an event listener
+    selectMenu.onchange = onTeamChanged;
+
+    ////this is an event handler
+    function onTeamChanged() {
+        // console.log('it works onTeamChanged');
+        let selectTeam = selectMenu.value;
+        console.log(`you changed your team to ${selectTeam}`);
+
+    }
 
 }
